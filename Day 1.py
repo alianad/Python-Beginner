1145ef97ded9bd763d0f84ff99f5b4a223a84d22
alianad <alia.nadhira29@gmail.com>
2025-02-04 16:43:41 +0800

    Initial Commit Day 1

---
+++ Day 1.py
@@ -0,0 +1,12 @@
+# --------------------- Project ------------------------------
+# Band Name Generator
+# ------------------------------------------------------------
+
+# --------------------- Objectives ---------------------------
+# Working with variables in Python to manage data
+# ------------------------------------------------------------
+
+print("Welcome to the Band Name Generator")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + city + " " + pet)
